4310e44aeeb61fcdbd1c0cf53f116c595f10b3ed
xyzeque <xyzequel@gmail.com>
2023-07-05 19:30:33 +0800

    Add solution to 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celsius = (temp - 32) * 5/9;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheit = temp * 9/5 + 32;
+  return fahrenheit;
 };
+
 
 // Do not edit below this line
 module.exports = {
