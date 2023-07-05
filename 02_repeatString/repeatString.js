8ed45b2287a3080aab22983935c25b3d29790c75
xyzeque <xyzequel@gmail.com>
2023-07-05 13:04:59 +0800

    Finish part 1 of 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let repeatedString = '';
+  for (let i = 0; i < num; i++) {
+    repeatedString += str;
+  }
+  return repeatedString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
