121fc66d189d4d08cc166d42ac1178dbeb815b54
xyzeque <xyzequel@gmail.com>
2023-07-05 19:00:38 +0800

    Add validation for other types to finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,14 +2,15 @@ const sumAll = function(a, b) {
   let min = Math.min(a,b);
   let max = Math.max(a,b);
   let sum = 0;
-  if (min >= 0 && max >= 0) {
+  if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+
+    return 'ERROR';
+  } else {
     for (let i = min; i <= max; i++) {
       sum += i;
     }
-    
+
     return sum;
-  } else {
-    return 'ERROR';
   }
 };
 
