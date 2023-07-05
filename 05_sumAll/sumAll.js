42ab323455051bd73796c30e1f7ca8d1375b8182
xyzeque <xyzequel@gmail.com>
2023-07-05 18:47:28 +0800

    Add solution that solves for the sum between two integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let min = Math.min(a,b);
+  let max = Math.max(a,b);
+  let sum = 0;
 
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
