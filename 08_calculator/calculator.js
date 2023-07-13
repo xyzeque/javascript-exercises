842a88dc1f7b39336cfe541ab6bd4ddc65aaf85c
xyzeque <xyzequel@gmail.com>
2023-07-13 09:31:33 +0800

    Add solution to factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -25,12 +25,21 @@ const multiply = function(...num) {
 
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	const power = Math.pow(base, exponent);
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
