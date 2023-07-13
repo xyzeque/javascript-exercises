fb7e5e6c533b026b22c2ca5b139782ab1ea05176
xyzeque <xyzequel@gmail.com>
2023-07-13 09:45:22 +0800

    Add palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // removes non-alphanumeric characters and converts to lowercase
+  const strModified = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  // reverses modified string
+  const strReversed = strModified.split('').reverse().join('');
+  // compares modified string with the reversed version
+  return strModified === strReversed;
 };
 
 // Do not edit below this line
