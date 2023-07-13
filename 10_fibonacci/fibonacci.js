8ffefd17a6fcd3bdb28097a6dfb80bbdbcf630a9
xyzeque <xyzequel@gmail.com>
2023-07-13 09:56:14 +0800

    Add fibonacci function solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function fibonacci(n) {
 
-};
+  // check if input is negative   
+  if (n <= 0) {
+    return 'OOPS';
+  }
+  // fibonacci sequence always starts with 1 1
+  if (n === '1' || n === '2') {
+    return 1;
+  }
+  
+  let a = 1;
+  let b = 1;
+  let result = 0;
+  
+  // calculates fibonacci number if input is greater than 3
+  for (let i = 3; i <= n; i++) {
+    result = a + b;
+    a = b;
+    b = result;
+  }
+  
+  return result;
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
